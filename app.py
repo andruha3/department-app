a38690efb2c4c4a10b4b8db337e9d754ac45a615
Andrii Kovalyk <andreykovalyk30@gmail.com>
2021-04-21 22:08:26 +0300

    Creating a simple page

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Home(Resource):
+    def get(self):
+        return 'Hello world'
+
+
+api.add_resource(Home, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
